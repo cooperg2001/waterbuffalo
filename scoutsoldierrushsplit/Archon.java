8eb071fea6e59f9421822b255c14302f86178230
Zach Obsniuk <pi.guy314@yahoo.com>
2017-01-12 16:38:19 -0500

    oops ignore the last one, it had the wrong package name

---
+++ scoutsoldierrushsplit/Archon.java
@@ -0,0 +1,84 @@
+package scoutsoldierrushsplit;
+
+import battlecode.common.*;
+
+/**
+ * Created by Zach on 1/12/2017.
+ */
+public class Archon {
+
+    static void runArchon(RobotController rc) throws GameActionException {
+
+
+        if(rc.readBroadcast(900) == 1){
+            int encode = (int)RobotPlayer.their_archons[0].x * 1000 + (int)RobotPlayer.their_archons[0].y;
+            rc.broadcast(500, encode);
+        }
+        Direction rand = RobotPlayer.randomDirection();
+        int saved_target_id = -1;
+
+        while (true) {
+
+            try {
+                //updateSurroundings();
+                RobotPlayer.findShakableTrees();
+                RobotPlayer.checkForStockpile();
+                if(rc.getRobotCount() > 40 || rc.getRoundNum() > 2800 || rc.getTeamBullets() > 10000 - 10 * rc.getTeamVictoryPoints()){
+                    rc.donate(rc.getTeamBullets() - (rc.getTeamBullets() % 10));
+                }
+
+                int data = rc.readBroadcast(500);
+                MapLocation last_seen = new MapLocation(data / 1000, data % 1000);
+                RobotInfo[] enemies = rc.senseNearbyRobots(-1, RobotPlayer.ENEMY);
+
+                if(enemies.length == 0){
+                    if(rc.getLocation().distanceTo(last_seen) < RobotPlayer.getSightRadius()){
+                        rc.broadcast(500, 0);
+                        rc.broadcast(501, 0);
+                    }
+                }
+                else{
+                    int encode = (int) enemies[0].getLocation().x * 1000 + (int) enemies[0].getLocation().y;
+                    rc.broadcast(500, encode);
+                    if(enemies[0].getID() != saved_target_id){
+                        saved_target_id = enemies[0].getID();
+                        rc.broadcast(501, 0);
+                    }
+                }
+
+
+                for(int i = 0; i < RobotPlayer.num_slices; i++){
+                    Direction dir = RobotPlayer.absolute_right.rotateLeftRads(RobotPlayer.potential_angles[i]);
+                    if(rc.canHireGardener(dir) && (rc.readBroadcast(904) >= 2 * rc.readBroadcast(901) - 3 || rc.getTeamBullets() > 200)){
+                        if(rc.getRoundNum() > 30){
+                            rc.hireGardener(dir);
+                        }
+                        else if(rc.canMove(dir.rotateLeftDegrees(90))){
+                            rc.hireGardener(dir);
+                            rc.move(dir.rotateLeftDegrees(90));
+                        }
+                        else if(rc.canMove(dir.rotateRightDegrees(90))){
+                            rc.hireGardener(dir);
+                            rc.move(dir.rotateRightDegrees(90));
+                        }
+                        break;
+                    }
+                }
+
+                int trials = 0;
+                while(!rc.canMove(rand) && trials < 10){
+                    rand = RobotPlayer.randomDirection();
+                    trials++;
+                }
+                if(rc.canMove(rand) && !rc.hasMoved()){
+                    rc.move(rand);
+                }
+
+                Clock.yield();
+            } catch (Exception e) {
+                System.out.println("Archon Exception");
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ scoutsoldierrushsplit/Gardener.java
@@ -0,0 +1,95 @@
+package scoutsoldierrushsplit;
+
+import battlecode.common.*;
+
+/**
+ * Created by Zach on 1/12/2017.
+ */
+public class Gardener {
+
+    static void runGardener(RobotController rc) throws GameActionException {
+
+
+        int spots_available = 0;
+        int saved_target_id = -1;
+
+        // The code you want your robot to perform every round should be in this loop
+        while (true) {
+
+            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
+            try {
+                //updateSurroundings();
+                //findShakableTrees();
+                int data = rc.readBroadcast(500);
+                MapLocation last_seen = new MapLocation(data / 1000, data % 1000);
+                RobotInfo[] enemies = rc.senseNearbyRobots(-1, RobotPlayer.ENEMY);
+
+                if(enemies.length == 0){
+                    if(rc.getLocation().distanceTo(last_seen) < RobotPlayer.getSightRadius()){
+                        rc.broadcast(500, 0);
+                    }
+                }
+                else{
+                    int encode = (int) enemies[0].getLocation().x * 1000 + (int) enemies[0].getLocation().y;
+                    rc.broadcast(500, encode);
+                    if(enemies[0].getID() != saved_target_id){
+                        saved_target_id = enemies[0].getID();
+                        rc.broadcast(501, 0);
+                    }
+                }
+
+                TreeInfo[] tree_list = rc.senseNearbyTrees(-1, rc.getTeam());
+                if(tree_list.length > 0){
+                    TreeInfo best = tree_list[0];
+                    for(int i = 0; i < tree_list.length; i++){
+                        if(tree_list[i].getHealth() < best.getHealth() && rc.canWater(tree_list[i].getLocation())){
+                            best = tree_list[i];
+                        }
+                    }
+                    if(rc.canWater(best.getLocation())){
+                        rc.water(best.getLocation());
+                    }
+                }
+
+                spots_available = 0;
+                for(int i = 0; i < RobotPlayer.num_gardener_slices; i++){
+                    Direction next_build = RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices);
+                    //System.out.println("Checking location... " + rc.getLocation().add(next_build,2) + " from " + rc.getLocation());
+                    if(rc.canMove(next_build, 2)){
+                        spots_available++;
+                    }
+                }
+
+                //System.out.println("There are " + spots_available + " spots available from " + rc.getLocation());
+
+                if(spots_available > 1 && rc.getRoundNum() > 40 && rc.getTreeCount() < 3 * rc.readBroadcast(904)){
+                    //System.out.println("Building tree...");
+                    for(int i = 0; i < RobotPlayer.num_gardener_slices; i++){
+                        if(rc.canPlantTree(RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices))){
+                            rc.plantTree(RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices));
+                        }
+                    }
+                }
+                else{
+                    for(int i = 0; i < RobotPlayer.num_gardener_slices; i++){
+                        if((rc.readBroadcast(903) < 20) && rc.canBuildRobot(RobotType.SOLDIER, RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices)) && (rc.readBroadcast(903) < rc.readBroadcast(904) + 1 || rc.getRoundNum() > 400)){
+                            rc.buildRobot(RobotType.SOLDIER, RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices));
+                        }
+                        if((rc.readBroadcast(904) < 20) && rc.canBuildRobot(RobotType.SCOUT, RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices)) && rc.readBroadcast(904) < 3 * rc.readBroadcast(903) + 3 && (rc.getRoundNum() < 400 || rc.getRoundNum() > 700)){
+                            //System.out.println("Building scout...");
+                            rc.buildRobot(RobotType.SCOUT, RobotPlayer.absolute_right.rotateLeftRads(i * 2 * (float)Math.PI/RobotPlayer.num_gardener_slices));
+                        }
+                    }
+                }
+
+
+                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+                Clock.yield();
+
+            } catch (Exception e) {
+                System.out.println("Gardener Exception");
+                e.printStackTrace();
+            }
+        }
+    }
+}
