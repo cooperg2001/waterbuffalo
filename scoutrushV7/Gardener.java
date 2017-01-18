4880462e42ded5700f0d8ac0abbeeb39fa9cae75
VeniVidiVici42 <bogtro123@gmail.com>
2017-01-18 16:40:37 -0500

    Update Gardener.java
    Small build priority changes

--- scoutrushV7/Gardener.java
+++ scoutrushV7/Gardener.java
@@ -1,15 +1,16 @@
 package scoutrushV7;
 
 import battlecode.common.*;
-import java.util.List;
-import java.util.ArrayList;
 
-
+/**
+ * Created by Zach on 1/12/2017.
+ */
 public class Gardener {
 
     static void runGardener(RobotController rc) throws GameActionException {
 
 
+        int spots_available = 0; // The number of places around our gardener we can build in
         boolean sentDeathSignal = false; // true if we've sent a death signal
 
         while(true){
@@ -28,12 +29,13 @@ public class Gardener {
 
 
 
-                if(RobotPlayer.our_trees.length > 0){
+                TreeInfo[] tree_list = rc.senseNearbyTrees(-1, rc.getTeam()); // Find all allied trees within range
+                if(tree_list.length > 0){
                     // Determine tree in range with least health
-                    TreeInfo best = RobotPlayer.our_trees[0];
-                    for(int i = 0; i < RobotPlayer.our_trees.length; i++){
-                        if(RobotPlayer.our_trees[i].getHealth() < best.getHealth() && rc.canWater(RobotPlayer.our_trees[i].getLocation())){
-                            best = RobotPlayer.our_trees[i];
+                    TreeInfo best = tree_list[0];
+                    for(int i = 0; i < tree_list.length; i++){
+                        if(tree_list[i].getHealth() < best.getHealth() && rc.canWater(tree_list[i].getLocation())){
+                            best = tree_list[i];
                         }
                     }
                     // Water the tree in range with least health, if one exists
@@ -43,36 +45,33 @@ public class Gardener {
                 }
 
                 // Determine the number of available build spots
+                spots_available = 0;
                 float gardener_angle_gradient = 2 * (float)Math.PI/RobotPlayer.num_angles;
 
 
                 boolean should_build_tree = false;
-                List<Integer> unoccupied_slots = new ArrayList<Integer>();
+                boolean[] occupied = new boolean[RobotPlayer.num_angles];
+                for(int i=0; i < RobotPlayer.num_angles; i++){
+                    occupied[i] = false;
+                }
                 for(int i=0; i < RobotPlayer.num_angles; i++){
                     Direction next_build = RobotPlayer.backward.rotateLeftRads(i*gardener_angle_gradient);
                     //System.out.println("Checking location... " + rc.getLocation().add(next_build,2) + " from " + rc.getLocation());
                     if(!rc.canMove(next_build, 2)){
-                        continue;
-                    }
-                    else{
-                        unoccupied_slots.add(i);
+                        occupied[i] = true;
                     }
                 }
-                if (unoccupied_slots.size() >= 6){
-                    should_build_tree = true;
-                }
-                else{
-                    for(int i = 0; i < unoccupied_slots.size(); i++) {
-                        for (int j = 0; j < unoccupied_slots.size(); j++) {
-                            int iVal = unoccupied_slots.get(i);
-                            int jVal = unoccupied_slots.get(j);
-                            int distance = Math.min(jVal - iVal, 24 - (jVal - iVal));
+                for(int i = 0; i < RobotPlayer.num_angles; i++){
+                    for(int j=0; j < RobotPlayer.num_angles; j++){
+                        if(!occupied[i] && !occupied[j]){
+                            int distance = Math.min(j - i, 24 - (j - i));
                             if (distance >= 5){
                                 should_build_tree = true;
                             }
                         }
                     }
                 }
+
 
                 if(should_build_tree && rc.getRoundNum() > 40 && (2 * rc.getTreeCount() + 1 < (rc.readBroadcast(904) + rc.readBroadcast(903)) || rc.getTeamBullets() > 100)){
                     // Leave a location open to build combat units in, and don't build a tree if our army is weak
@@ -106,22 +105,23 @@ public class Gardener {
                             next_build = RobotPlayer.forward.rotateRightRads((int)(i + 1)/2 * gardener_angle_gradient);
                         }
 
-                        if( numScouts + numSoldiers - 6 > 4 * numLumberjacks
+                        if(numLumberjacks < 2
                                 && rc.canBuildRobot(RobotType.LUMBERJACK, next_build)
-                                && RobotPlayer.neutral_trees.length > 0){
+                                && numScouts + numSoldiers >= 2
+                                && (RobotPlayer.neutral_trees.length > 0){
                             rc.buildRobot(RobotType.LUMBERJACK, next_build);
                             rc.broadcast(902, rc.readBroadcast(902) + 1);
                         }
                         if((numSoldiers < 20)
                                 && rc.canBuildRobot(RobotType.SOLDIER, next_build)
-                                && (numSoldiers < numScouts + 1 || rc.getRoundNum() > 400)){
+                                && (numSoldiers < numScouts || rc.getRoundNum() > 400)){
                             rc.buildRobot(RobotType.SOLDIER, next_build);
                             rc.broadcast(904, rc.readBroadcast(904) + 1);
                         }
                         if((numScouts < 20)
                                 && rc.canBuildRobot(RobotType.SCOUT, next_build)
                                 && (numScouts < 2 * numSoldiers + 1)
-                                && (rc.getRoundNum() < 400 || rc.getRoundNum() > 700)){
+                                && (rc.getRoundNum() < 400)){
                             //System.out.println("Building scout...");
                             rc.buildRobot(RobotType.SCOUT, next_build);
                             rc.broadcast(903, rc.readBroadcast(903) + 1);
